1e8665fcd7c996ad0750588b6849b0bc87501d5c
maraouine <raouine.mohamed2004@gmail.com>
2020-03-06 00:09:11 +0100

    MOVESnake

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ window.onload = function()
         canvas.style.border="1px solid";
         document.body.appendChild(canvas);
         ctx= canvas.getContext('2d'); 
-        snakee=new snake([[6,4],[5,4],[4,4]]);
+        snakee=new snake([[6,4],[5,4],[4,4]],"right");
         refreshCanvas();
 
     }
@@ -40,9 +40,13 @@ window.onload = function()
         
     }
    
-    function snake(body)
+    
+    /*Construteur*/
+    
+    function snake(body,direction)
     {
         this.body=body;
+        this.direction=direction;
         this.draw=function()
         {
           ctx.save();
@@ -57,10 +61,93 @@ window.onload = function()
         this.advance=function()
         {
             var nextPosition = this.body[0].slice();
-            nextPosition[0]+=1;
+            
+            console.log(this.direction);
+            switch (this.direction)
+                {
+                    
+                    case "left":
+                        nextPosition[0]-=1;
+
+                        break;
+                    case "right":
+                        nextPosition[0]+=1;
+                        break;
+                    case "down":
+                         nextPosition[1]+=1;
+
+                        break;
+                    case "up":
+                         nextPosition[1]-=1;
+                        break;
+                    default:
+                        throw("invalid direction");
+                }
             this.body.unshift (nextPosition);
             this.body.pop();
         };
         
+        this.setDirection = function(newDirection)
+        {
+            
+            var allowedDirection;
+            switch (this.direction)
+                {
+                    case "left":
+                    case "right":
+                        allowedDirection=["up","down"];
+                    break;
+                    case "down":
+                    case "up":
+                     allowedDirection=["left","right"];
+                        break;
+                    default:
+                        throw("invalid direction");
+                        
+                }
+
+            console.log(allowedDirection.indexOf(newDirection));
+            if(allowedDirection.indexOf(newDirection) > -1)
+                {
+                    this.direction=newDirection;
+                    
+                    console.log(this.direction);
+                }
+        };
+        
     }
+    document.onkeydown= function handlekeydown(e)
+    {
+        var key =e.keyCode;
+        var newDirection;
+        
+        switch(key)
+            {
+                case 37 :
+                    newDirection="left";
+                    console.log(key);
+                    break;
+                case 38 :
+                    newDirection="up";
+                                        console.log(key);
+
+                    break;
+                case 39 :
+                    newDirection="right";
+                                        console.log(key);
+
+                    break;
+                case 40 :
+                    newDirection="down";
+                                        console.log(key);
+
+                break;
+                    default:
+                        return;
+            }
+        snakee.setDirection(newDirection);
+    }
+    
+ 
 }
+   
